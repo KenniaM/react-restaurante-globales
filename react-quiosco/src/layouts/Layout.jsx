30b03415bbe0db8496b567d5124b7bc72fdeb2cb
KenniaM <kennia.martinez.camacho@est.una.ac.cr>
2024-09-11 16:16:21 -0600

    Ventana modal para agregar un Producto y Cantidades

--- react-quiosco/src/layouts/Layout.jsx
+++ react-quiosco/src/layouts/Layout.jsx
@@ -1,9 +1,30 @@
 import { Outlet } from 'react-router-dom'
+import Modal from 'react-modal'
 import Sidebar from '../components/Sidebar'
 import Resumen from '../components/Resumen'
+import useQuisco from '../hooks/useQuiosco'
+import ModalProducto from '../components/ModalProducto'
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+Modal.setAppElement('#root')
 
 export default function Layout() {
+
+  const { modal, handleClickModal } = useQuisco();
+  console.log(modal)
+
   return (
+    <>
     <div className='md:flex'>
       <Sidebar />
 
@@ -13,5 +34,13 @@ export default function Layout() {
 
       <Resumen />
     </div>
+
+      <Modal isOpen={modal} style={customStyles}>
+        <ModalProducto/>
+        
+      </Modal>
+
+
+    </>
   )
 }
